ce281ee899fec5df12df80ce16c4cdab72379755
zyz <zyzisgood@gmail.com>
2020-04-05 21:35:22 -0500

    update head component

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {CSSTransition} from 'react-transition-group';
 
 import {
     HeaderWrapper,
@@ -7,12 +8,31 @@ import {
     NavItem,
     NavSearch,
     Addition,
-    Button
+    Button,
+    SearchWrapper,
 } from './style';
 
 
 class Header extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            focused:false
+        }
+        this.handleInputFocus = this.handleInputFocus.bind(this);
+        this.handleInputBlur = this.handleInputBlur.bind(this);
+    }
+    handleInputFocus(){
+        this.setState({
+            focused:true
+        })
+    }
+    handleInputBlur(){
+        this.setState({
+            focused:false
+        })
+    }
     render(){
         return (
             <HeaderWrapper>
@@ -21,11 +41,29 @@ class Header extends Component {
                     <NavItem className = 'left active'>首页</NavItem>
                     <NavItem className = 'left'>下载App</NavItem>
                     <NavItem className ='right'>登录</NavItem>
-                    <NavItem className = 'right'>Aa</NavItem>
-                    <NavSearch></NavSearch>
+                    <NavItem className = 'right'>
+                        <i className='iconfont'>&#xe636;</i>
+                    </NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.state.focused}
+                            timeout = {200}
+                            classNames="slide"
+                        >
+                            <NavSearch 
+                                className={this.state.focused?'focused':''}
+                                onFocus = {this.handleInputFocus}
+                                onBlur = {this.handleInputBlur}
+                                ></NavSearch>
+                        </CSSTransition>
+                        <i className={this.state.focused?'focused iconfont':'iconfont'}>&#xe614;</i>
+                    </SearchWrapper>
                 </Nav>
                 <Addition>
-                    <Button className='writting'>写文章</Button>
+                    <Button className='writting'>
+                       <i className='iconfont'>&#xe615;</i>
+                        写文章
+                    </Button>
                     <Button className='reg'>注册</Button>
                 </Addition>
             </HeaderWrapper>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './style.js';
+import GlobalStyle1 from './statics/iconfont/iconfont';
 import App from './App';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle /> 
+    <GlobalStyle />
+    <GlobalStyle1 />  
     <App />
   </React.StrictMode>,
   document.getElementById('root')
