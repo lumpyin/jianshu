829f7aa0b4887a06480b02465f73cd203851ef94
zyz <zyzisgood@gmail.com>
2020-04-12 13:03:48 -0500

    home page components create

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Header from './common/header';
+import {BrowserRouter,Route} from 'react-router-dom';
 import store from './store';
 import {Provider} from 'react-redux';
-
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 
 function App() {
   return (
     <Provider store={store}>
+      <>
         <Header />
+        <BrowserRouter>
+         <>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/detail' exact component={Detail}></Route>
+         </>
+        </BrowserRouter>
+       </> 
     </Provider>
    
   );
