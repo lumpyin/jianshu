12302ee808dc5db1469375552b7c8de95e3f7115
zyz <zyzisgood@gmail.com>
2020-04-12 19:54:19 -0500

    update page component

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -9,11 +9,30 @@ const defaultState = fromJS({
 	showScroll: false
 });
 
-export default (state = defaultState,action)=> {
-    switch(action.type){
-           
-        default:
-            return state;
-    }
-    
+const changeHomeData = (state, action) => {
+	return state.merge({
+		topicList: fromJS(action.topicList),
+		articleList: fromJS(action.articleList),
+		recommendList: fromJS(action.recommendList)
+	});
+};
+
+const addArticleList = (state, action) => {
+	return state.merge({
+		'articleList': state.get('articleList').concat(action.list),
+		'articlePage': action.nextPage
+	});
+};
+
+export default (state = defaultState, action) => {
+	switch(action.type) {
+		case constants.CHANGE_HOME_DATA:
+			return changeHomeData(state, action);
+		case constants.ADD_ARTICLE_LIST:
+			return addArticleList(state, action);
+		case constants.TOGGLE_SCROLL_TOP:
+			return state.set('showScroll', action.show);
+		default:
+			return state;
+	}
 }
